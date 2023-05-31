42a751a7eb975a879a5c7f153cb5649941f4eed8
Andreas242 <andreas.ahlgren@soprasteria.com>
2023-05-31 08:46:41 +0200

    buildfix

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,13 @@
 import styles from '@/styles/Home.module.css'
 import Wizard from './wizard'
 
-const steps = [{ id: 'step1', label: 'Step 1', icon: 'icon1' },
-{ id: 'step2', label: 'Step 2', icon: 'icon2' },
-// Add dynamic steps 
-{ id: 'final', label: 'Final Step', icon: 'iconFinal' },
-];
+
 
 export default function Home() {
   return (
     <>
       <div className={'wrapper'}>
-        <Wizard steps={steps} />
+        <Wizard />
       </div>
     </>
   )
