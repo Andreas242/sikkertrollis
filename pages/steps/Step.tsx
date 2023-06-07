259e5cbc562dc258b75cbabcb74ab042630341f6
Andreas242 <andreas.ahlgren@soprasteria.com>
2023-06-07 10:23:44 +0200

    break_out

--- pages/steps/Step.tsx
+++ pages/steps/Step.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useMemo, useContext } from "react";
 import { useState } from "react";
 import styles from "../../styles/Icons.module.css";
 import { WizardContext } from "../index";
+import { Anbefaling } from "@/components/Anbefaling";
 
 interface Option {
   id: number;
@@ -39,36 +40,26 @@ const Step: React.FC<StepProps> = ({
     <div>
       {content &&
         <div key={step}>
-          <h2>{content.HEADING}</h2>          
+          <h2>{content.HEADING}</h2>
           <div className={styles.responseContainer}>
             <div className={styles.responseContent}>
-              
               {options.map((option) => (
                 <div key={option.id} className={styles.responseOption}>
                   <label>
-                  <input
-                    type="radio"
-                    value={option.id}
-                    checked={state[step] === option.id.toString()}
-                    onChange={handleOptionChange}
-                  />
-                  <span className={styles.stepOptions}>{option.label}</span>
+                    <input
+                      type="radio"
+                      value={option.id}
+                      checked={state[step] === option.id.toString()}
+                      onChange={handleOptionChange}
+                    />
+                    <span className={styles.stepOptions}>{option.label}</span>
                   </label>
                 </div>
               ))}
             </div>
             {!selectedOption && <div className={styles.emptyrecommendation}></div>}
-            {selectedOption && <div className={styles.recommendation}>
-              <h3>Anbefaling</h3>
-              {selectedOption === "1" && <div>Dere har en del å jobbe med fremover</div>}
-              {selectedOption === "2"  && <div>Dere har en del å jobbe med fremover</div>}
-              {selectedOption === "3" && <div>Bra at dere har startet med tiltak, men det er mer å jobbe med</div>}
-              {selectedOption >= "4" && <div>Suverent! Dere er godt forberedt på denne trusselen</div>}
-              {selectedOption === "5" && <div></div>}
-              <div>
-                Poeng: {selectedOption}
-              </div>
-            </div>
+            {selectedOption &&
+              <Anbefaling />
             }
           </div>
         </div>
